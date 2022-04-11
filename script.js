8209ba70bd4f5b9ba9265433d1819f77a0d0a700
Kindstep <kindstep@arcada.fi>
2022-04-11 23:29:53 +0300

    3 fungerande api-widgets

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const IP_URL = "https://cgi.arcada.fi/~kindstep/Startsida/wdbcms22-projekt-1-hardtimez/api/ip/";
+
+const Joke_URL = "https://v2.jokeapi.dev";
+const categories = ["Programming", "Misc", "Pun", "Spooky", "Christmas"];
+const params = [
+    "blacklistFlags=religious",
+    "idRange=0-150"
+];
+
 
 
 function settings() {
@@ -7,7 +16,72 @@ function settings() {
     localStorage.setItem("apiKey", apiKey);
 }
 
+async function getIP() {
+    // Kalla på vår php-IP-API med fetch()
+    const resp = await fetch(IP_URL);
+    // Ta emot response och omvandla till json
+    const respBody = await resp.json();
+
+    document.querySelector("#ip").innerText = respBody.ip; 
+}
+
+
+
+async function getJoke() {
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", Joke_URL + "/joke/" + categories.join(",") + "?" + params.join("&"));
+
+    xhr.onreadystatechange = function() {
+     if(xhr.readyState == 4 && xhr.status < 300) // readyState 4 means request has finished + we only want to parse the joke if the request was successful (status code lower than 300)
+         {
+             var randomJoke = JSON.parse(xhr.responseText);
+
+        if(randomJoke.type == "single")
+        {
+            document.querySelector("#joke").innerText = randomJoke.joke; 
+            
+        }
+        else
+        {
+            document.querySelector("#joke").innerText = randomJoke.setup; 
+            document.querySelector("#joke2").innerText = randomJoke.delivery; 
+        }
+    }
+    else if(xhr.readyState == 4)
+    {
+        document.querySelector("#joke").innerText = ("Error while requesting joke.\n\nStatus code: " + xhr.status + "\nServer response: " + xhr.responseText);
+    }
+};
+
+xhr.send();
+//Källa: https://sv443.net/jokeapi/v2/#wrappers, för att få apin till att funka
+}
+
+async function getActivity()
+{
+        const resp = await fetch( "https://www.boredapi.com/api/activity/", {
+          headers: {
+            Accept: "application/json",
+          },
+        });
+            const data = await resp.json();
+
+    document.querySelector("#activity").innerText = data.activity;
+}
+
 
 
 // Event listeners
+getIP();
+getJoke();
+getActivity();
+
 document.getElementById("settings").addEventListener("click", settings);
+
+
+
+
+
+
+
