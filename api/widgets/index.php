4e0962c872a13a4b74cb3520492da60385cc71d4
attelah <lahepela@arcada.fi>
2022-04-25 22:21:45 +0300

    api key 90% färdig

--- api/widgets/index.php
+++ api/widgets/index.php
@@ -3,44 +3,48 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-    require_once("config.php");
+require_once "config.php";
 
-    try {
-      $conn_string = "pgsql:host=" . $db_conf['host'] . ";port=5432;dbname=" . $db_conf['dbname'];
-      $pdo = new PDO($conn_string, $db_conf['user'], $db_conf['password'], [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+try {
+    $conn_string = "pgsql:host=" . $db_conf['host'] . ";port=5432;dbname=" . $db_conf['dbname'];
+    $pdo = new PDO($conn_string, $db_conf['user'], $db_conf['password'], [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 //      echo "Connection works!";
-    } catch (PDOException $e) {
-      die($e->getMessage());
-    }
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
 
-    // Berätta år browsern att vi tänker skicka JSON-data
-    header("Content-Type: application/json");
+// Berätta år browsern att vi tänker skicka JSON-data
+header("Content-Type: application/json");
 
-    // Vi plockar ut variablerna från URLen och sparar i $request_vars
-    parse_str($_SERVER['QUERY_STRING'], $request_vars); // ==> ARRAY
+// Vi plockar ut variablerna från URLen och sparar i $request_vars
+parse_str($_SERVER['QUERY_STRING'], $request_vars); // ==> ARRAY
 
-    // vi plockar ut data från request-bodyn 
-    $request_json = file_get_contents('php://input');
-    $request_body = json_decode($request_json); // ==> OBJEKT
-    // Vi kan casta (byta datatyp) från objekt till array så här: 
-    $request_body_arr = (array) $request_body;
+// vi plockar ut data från request-bodyn
+$request_json = file_get_contents('php://input');
+$request_body = json_decode($request_json); // ==> OBJEKT
+// Vi kan casta (byta datatyp) från objekt till array så här:
+$request_body_arr = (array) $request_body;
+// Alla headers
+$req_headers = getallheaders();
 
 // Vår response först som en PHP-array
 
 
-// Här kollar vi att alla requests som inte är GET har valid API_key, annars exit.
-// $api_key får sitt värde i hotel_config.php!
-if ($_SERVER['REQUEST_METHOD'] != "GET" 
-    && (!isset($req_headers['x-api-key']) || $req_headers['x-api-key'] != $api_key)
-  ) {
+if (isset($req_headers['x-api-key'])) {
+$stmt = $pdo->prepare("SELECT * FROM users WHERE api_key = ?");
+$stmt->execute([$req_headers['x-api-key']]);
+$dbresult = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    echo json_encode(["msg" => "BAD KEY"]);
+}
+// Här kollar vi att alla requests har valid API_key, annars exit.
+if ($_SERVER['REQUEST_METHOD'] == "GET" && (!isset($req_headers['x-api-key']) || $req_headers['x-api-key'] != $dbresult['api_key'])) {
+
+    echo json_encode(["error" => "403"]);
     exit();
+} else {
+    echo json_encode(["username" => $dbresult]);
 }
 
-    // Omvandla PHP-arrayen till JSON och skriv ut
-    echo json_encode($response);
-
-
-
+// Omvandla PHP-arrayen till JSON och skriv ut
+//    echo json_encode($response);
 
--- script.js
+++ script.js
@@ -1,24 +1,33 @@
+const API_URL = "https://cgi.arcada.fi/~lahepela/wdbcms22-projekt-1-hardtimez/api/widgets"
 const IP_URL = "https://cgi.arcada.fi/~kindstep/Startsida/wdbcms22-projekt-1-hardtimez/api/ip/";
 
-const Joke_URL = "https://v2.jokeapi.dev";
-const categories = ["Programming", "Misc", "Pun", "Spooky", "Christmas"];
-const params = [
-    "blacklistFlags=religious",
-    "idRange=0-250"
-];
 
 //////////// API API API API ////////////
-const URL = "https://cgi.arcada.fi/~lahepela/wdbcms22-projekt-1-hardtimez/api/widgets"
+
+getWidgets();
 async function getWidgets() {
-  const resp = await fetch(URL, {
-    method: 'CHECKAPIKEY',
-    headers: { 'x-api-key': localStorage.getItem("apiKey") }
+  const resp = await fetch(API_URL, {
+    method: 'GET',
+    headers: {
+      'x-api-key': localStorage.getItem("apiKey")
+    }
   });
   const respData = await resp.json();
+  if (respData['error'] == "403") {
+    console.log("invalid api key");
+    document.querySelector('#errorText').innerHTML = "Invalid API key";
+  } else {
+    document.querySelector('#errorText').innerHTML = "";
+    // run functions that require API key
+    getIP(respData['widgets'][0]);
+    getCat(respData['widgets'][0]);
+    console.log(respData['widgets']['cat']);
+  }
 }
 
 function applyApi() {
-    localStorage.setItem("apiKey", document.querySelector('#apiKey').value);
+  localStorage.setItem("apiKey", document.querySelector('#apiKey').value);
+  getWidgets();
 }
 // Fyller i APIKEYN färdigt i fältet
 if (localStorage.getItem("apiKey")) {
@@ -30,84 +39,74 @@ if (localStorage.getItem("apiKey")) {
 
 
 
-async function getIP() {
-    fetch("https://ipinfo.io/json?token=fd434b7101caae").then(
-        (response) => response.json()
-      ).then(
-        (jsonResponse) => document.querySelector("#ip").innerText = jsonResponse.ip + ("\n") + jsonResponse.country + (", ") +jsonResponse.city+"\n GPS Coordinates: "+jsonResponse.loc);
-      
+async function getIP(key) {
+  fetch("https://ipinfo.io/json?token=" + key).then(
+    (response) => response.json()
+  ).then(
+    (jsonResponse) => document.querySelector("#ip").innerText = jsonResponse.ip + ("\n") + jsonResponse.country + (", ") + jsonResponse.city + "\n GPS Coordinates: " + jsonResponse.loc);
+
 }
 
 
 async function getJoke() {
 
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", Joke_URL + "/joke/" + categories.join(",") + "?" + params.join("&"));
+  const Joke_URL = "https://v2.jokeapi.dev";
+  const categories = ["Programming", "Misc", "Pun", "Spooky", "Christmas"];
+  const params = [
+    "blacklistFlags=religious",
+    "idRange=0-250"
+  ];
 
-    xhr.onreadystatechange = function() {
-     if(xhr.readyState == 4 && xhr.status < 300) 
-         {
-             var randomJoke = JSON.parse(xhr.responseText);
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", Joke_URL + "/joke/" + categories.join(",") + "?" + params.join("&"));
 
-        if(randomJoke.type == "single")
-        {
-            document.querySelector("#joke").innerText = randomJoke.joke; 
-            
-        }
-        else
-        {
-            document.querySelector("#joke").innerText = randomJoke.setup; 
-            document.querySelector("#joke2").innerText = randomJoke.delivery; 
-        }
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status < 300) {
+      var randomJoke = JSON.parse(xhr.responseText);
+
+      if (randomJoke.type == "single") {
+        document.querySelector("#joke").innerText = randomJoke.joke;
+
+      } else {
+        document.querySelector("#joke").innerText = randomJoke.setup;
+        document.querySelector("#joke2").innerText = randomJoke.delivery;
+      }
+    } else if (xhr.readyState == 4) {
+      document.querySelector("#joke").innerText = ("Error while requesting joke.\n\nStatus code: " + xhr.status + "\nServer response: " + xhr.responseText);
     }
-    else if(xhr.readyState == 4)
-    {
-        document.querySelector("#joke").innerText = ("Error while requesting joke.\n\nStatus code: " + xhr.status + "\nServer response: " + xhr.responseText);
-    }
-};
+  };
 
-xhr.send();
-//Källa: gjort enligt https://sv443.net/jokeapi/v2/#wrappers instruktioner, Gjort med XHR för att få apin till att funka.
+  xhr.send();
+  //Källa: gjort enligt https://sv443.net/jokeapi/v2/#wrappers instruktioner, Gjort med XHR för att få apin till att funka.
 }
 
-async function getActivity()
-{
-        const resp = await fetch( "https://www.boredapi.com/api/activity/", {
-          headers: {
-            Accept: "application/json",
-          },
-        });
-            const data = await resp.json();
-
-    document.querySelector("#activity").innerText = data.activity;
-}
-
-// TODO hide APIKEY sen när apikey databas skite funkar
-async function getCat() {
-  const resp = await fetch( "https://api.thecatapi.com/v1/images/search?api_key=22f8569d-b07f-4144-8e33-6fd2ca3af43a", {
+async function getActivity() {
+  const resp = await fetch("https://www.boredapi.com/api/activity/", {
     headers: {
       Accept: "application/json",
     },
   });
-      const data = await resp.json();
-      console.log(data[0].url);
+  const data = await resp.json();
 
-document.querySelector("#catApi").src = data[0].url;
+  document.querySelector("#activity").innerText = data.activity;
+}
+
+// TODO hide APIKEY sen när apikey databas skite funkar
+async function getCat(key) {
+  const resp = await fetch("https://api.thecatapi.com/v1/images/search?api_key=" + key, {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const data = await resp.json();
+  console.log(data[0].url);
+
+  document.querySelector("#catApi").src = data[0].url;
 }
 
 
 
-// Event listeners
-getIP("ipinfo.io?token=$TOKEN");
+// Event listeners & functions that don't require API key
 getJoke();
 getActivity();
-getCat();
-
 document.getElementById("applyApi").addEventListener("click", applyApi);
-
-
-
-
-
-
-
